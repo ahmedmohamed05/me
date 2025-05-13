42d11c2c7b0297b834be0432250dce91e07b3ae4
Ahmed Mohamed <ahmedmohuammed.dev@gmail.com>
2025-05-14 02:45:38 +0300

    add sudoku to the works section and the technologies of the project

--- src/content/works.ts
+++ src/content/works.ts
@@ -1,7 +1,4 @@
 import { type Tech } from "./skills";
-// import { xo1 } from "../images/xo-1.png";
-// import xo2 from "../images/xo-2.png";
-// import xo3 from "../images/xo-3.png";
 
 export interface Work {
   title: string;
@@ -13,28 +10,56 @@ export interface Work {
 }
 
 export const works: Work[] = [
+  // {
+  //   title: "AI XO Game",
+  //   description:
+  //     "Simple XO game, where you can play against the computer (Simple AI) or your friend locally",
+  //   images: ["/images/xo-1.png", "/images/xo-2.png", "/images/xo-3.png"],
+  //   link: "https://ahmedmohamed05.github.io/xo/",
+  //   repo: "https://github.com/ahmedmohamed05/xo",
+  //   technologies: [
+  //     {
+  //       name: "HTML",
+  //       icon: "/icons/html.svg",
+  //     },
+  //     {
+  //       name: "CSS",
+  //       icon: "/icons/css.svg",
+  //     },
+  //     {
+  //       name: "Javascript",
+  //       icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+  //     },
+  //   ],
+  // },
+
   {
-    title: "AI XO Game",
-    description:
-      "Simple XO game, where you can play against the computer (Simple AI) or your friend locally",
-    images: ["/images/xo-1.png", "/images/xo-2.png", "/images/xo-3.png"],
-    link: "https://ahmedmohamed05.github.io/xo/",
-    repo: "https://github.com/ahmedmohamed05/xo",
+    title: "Sudoku",
+    description: "Sudoku puzzle with 4 different difficulty levels",
+    images: [
+      "/images/sudoku-1.png",
+      "/images/sudoku-2.png",
+      "/images/sudoku-3.png",
+      "/images/sudoku-4.png",
+    ],
+    link: "https://ahmedmohamed05.github.io/sudoku/",
+    repo: "https://github.com/ahmedmohamed05/sudoku",
     technologies: [
       {
-        name: "HTML",
-        icon: "/icons/html.svg",
+        name: "React",
+        icon: "/icons/react.svg",
       },
       {
-        name: "CSS",
-        icon: "/icons/css.svg",
+        name: "Ts",
+        icon: "/icons/ts.svg",
       },
       {
-        name: "Javascript",
-        icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg",
+        name: "Tailwind CSS",
+        icon: "/icons/tailwind.svg",
       },
     ],
   },
+
   {
     title: "True Footballer",
     description:
@@ -46,6 +71,7 @@ export const works: Work[] = [
       "/images/true-footballer-4.png",
     ],
     repo: "https://github.com/ahmedmohamed05/true-footballer",
+    link: "https://ahmedmohamed05.github.io/clone-google/",
     technologies: [
       {
         name: "C++",
@@ -54,4 +80,23 @@ export const works: Work[] = [
       { name: ".NET", icon: "/icons/dotnet.svg" },
     ],
   },
+
+  // {
+  //   title: "Google Clone",
+  //   description: "Google homepage clone with html css",
+  //   images: [
+  //     "/images/true-footballer-1.png",
+  //     "/images/true-footballer-2.png",
+  //     "/images/true-footballer-3.png",
+  //     "/images/true-footballer-4.png",
+  //   ],
+  //   repo: "https://github.com/ahmedmohamed05/clone-google ",
+  //   technologies: [
+  //     {
+  //       name: "C++",
+  //       icon: "/icons/cpp.svg",
+  //     },
+  //     { name: ".NET", icon: "/icons/dotnet.svg" },
+  //   ],
+  // },
 ];
