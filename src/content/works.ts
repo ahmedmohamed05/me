8da506d8207b83abca01d9e370d54e0f3960d48a
Ahmed Mohamed <ahmedmohuammed.dev@gmail.com>
2025-09-23 18:37:13 -0300

    adding chess project to the works section, edit some styles

--- src/content/works.ts
+++ src/content/works.ts
@@ -87,6 +87,32 @@ export const works: Work[] = [
   },
 
   {
+    title: "Chess Game",
+    description: "Simple Chess Game Build Pure React + Ts",
+    images: [
+      "/images/chess-1.png",
+      "/images/chess-2.png",
+      "/images/chess-3.png",
+    ],
+    link: "https://chess-j.netlify.app/",
+    repo: "https://github.com/ahmedmohamed05/chess",
+    technologies: [
+      {
+        name: "React",
+        icon: "/icons/react.svg",
+      },
+      {
+        name: "Ts",
+        icon: "/icons/ts.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        icon: "/icons/tailwind.svg",
+      },
+    ],
+  },
+
+  {
     title: "True Footballer",
     description:
       "A Football quiz desktop app with simple data-base for the accounts and scores",
